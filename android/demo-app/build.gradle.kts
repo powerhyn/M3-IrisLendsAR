594e43948e43f1b9d327d03129c0986f41411efc
MerooMong <powerhyn@gmail.com>
2026-03-12 12:30:56 +0900

    기타 항목 커밋

--- android/demo-app/build.gradle.kts
+++ android/demo-app/build.gradle.kts
@@ -19,7 +19,7 @@ android {
         applicationId = "com.irislenssdk.demo"
         minSdk = 24
         targetSdk = 34
-        versionCode = 194
+        versionCode = 196
         versionName = "1.0.1"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
